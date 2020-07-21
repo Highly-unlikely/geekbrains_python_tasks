1fd46ed65bcdf64f2cdf34ec7d398dd7868109a1
Highly-unlikely <tripod.ollie@gmail.com>
2020-07-21 14:41:26 +0300

    3 tasks for lesson 1

---
+++ lesson_1/task_3/task_3.py
@@ -0,0 +1,23 @@
+# 3. Написать программу, которая генерирует в указанных пользователем границах:
+# a. случайное целое число,
+# b. случайное вещественное число,
+# c. случайный символ
+
+import random
+print('Введите диапазон для целого числа')
+min_int = int(input('Введите начало диапазона: '))
+max_int = int(input('Введите конец диапазона: '))
+
+print('Введите диапазон для вещественного числа')
+min_float = float(input('Введите начало диапазона: '))
+max_float = float(input('Введите конец диапазона: '))
+
+print('Введите диапазон для символа')
+min_char = str(input('Введите начало диапазона: '))
+max_char = str(input('Введите конец диапазона: '))
+
+print('Случайное целое число: ' + str(random.randint(min_int, max_int)))
+print('Случайное вещественное число: ' + str(random.uniform(min_float, max_float)))
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+print('Случайный символ: ' + alphabet[random.randrange(alphabet.find(min_char), alphabet.find(max_char))])
